db3b4290acdd1c034c208ebd539d90cf7cd182cf
Andre Susantin <andre.susantin@google.com>
2021-02-22 17:10:24 -0800

    hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -2,4 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('strands not equal in length')
 
-    return sum(a != b for a,b in zip(list(strand_a),list(strand_b)))
+    return sum(a != b for a,b in zip(strand_a,strand_b))
